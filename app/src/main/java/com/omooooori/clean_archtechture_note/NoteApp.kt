65a13333316e8f4b8bc3ee0b899501fa731886b4
omooooori <r.shimizu18020@gmail.com>
2021-11-03 17:57:20 +0900

    Implementation di using dagger hilt.

---
+++ app/src/main/java/com/omooooori/clean_archtechture_note/NoteApp.kt
@@ -0,0 +1,8 @@
+package com.omooooori.clean_archtechture_note
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application() {
+}
---
+++ app/src/main/java/com/omooooori/clean_archtechture_note/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,6 @@
+package com.omooooori.clean_archtechture_note.feature_note.domain.use_case
+
+data class NoteUseCases(
+    val getNotes: GetNotes,
+    val deleteNote: DeleteNote
+)
