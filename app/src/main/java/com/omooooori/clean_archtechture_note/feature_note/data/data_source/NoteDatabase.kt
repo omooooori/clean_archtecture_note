6ac300929d7d16c8b2557e9ea961313a6bfb16d0
omooooori <r.shimizu18020@gmail.com>
2021-11-03 12:05:45 +0900

    Inherit the RoomDatabase from NoteDatabase class.

--- app/src/main/java/com/omooooori/clean_archtechture_note/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/omooooori/clean_archtechture_note/feature_note/data/data_source/NoteDatabase.kt
@@ -1,6 +1,7 @@
 package com.omooooori.clean_archtechture_note.feature_note.data.data_source
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.omooooori.clean_archtechture_note.feature_note.domain.model.Note
 
 @Database(
@@ -8,8 +9,11 @@ import com.omooooori.clean_archtechture_note.feature_note.domain.model.Note
     version = 1
 )
 
-abstract class NoteDatabase {
+abstract class NoteDatabase: RoomDatabase() {
 
     abstract val noteDao: NoteDao
 
+    companion object {
+        const val DATABASE_NAME = "note_db"
+    }
 }
